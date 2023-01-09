d8451d8690d2da410d897e75e72bf12207a84585
AbmSourav <keramotul.islam@gmail.com>
2023-01-09 15:31:44 +0600

    data mapping updated

--- Core/PrepareData.php
+++ Core/PrepareData.php
@@ -5,6 +5,8 @@ namespace Codesvault\WPseeder\Core;
 class PrepareData
 {
     private $data = [];
+    private $row_count;
+    private $table_name;
 
     public function map()
     {
@@ -25,20 +27,25 @@ class PrepareData
             }
             require(WPS_ROOT_DIR . "/seeders/" . $seeder . ".php");
             $seeder_object = new $seeder;
-            $this->data[] = $seeder_object->run();
-            $this->innerLoop($seeder, $seeder_object->row);
+            $table = WPS_TABLE_PREFIX . $seeder_object->table;
+            print_r("\n\033[32m Generating data for " . $table . " table...\033[0m");
+
+            $this->row_count = $seeder_object->row;
+            $this->table_name = $seeder_object->table;
+            $this->data[$seeder_object->table][] = $seeder_object->run();
+            $this->innerLoop($seeder);
         }
        return $this->data;
     }
 
-    private function innerLoop($seeder, $row)
+    private function innerLoop($seeder)
     {
-        if (1 === $row || empty($this->data)) {
+        if (1 === $this->row_count || empty($this->data)) {
             return;
         }
 
-        for ($i = 1; $i < $row; $i++) {
-            $this->data[] = (new $seeder)->run();
+        for ($i = 1; $i < $this->row_count; $i++) {
+            $this->data[$this->table_name][] = (new $seeder)->run();
         }
     }
 }
--- Core/WPSeeder.php
+++ Core/WPSeeder.php
@@ -6,7 +6,17 @@ use Codesvault\WPseeder\Core\Api\Seeder;
 
 class WPSeeder implements Seeder
 {
+    /**
+     * Number of rows to create in the DB.
+     * Default is 1.
+     */
     public $row = 1;
+
+    /**
+     * Database table name without prefix.
+     * Default is 'posts'.
+     */
+    public $table = "posts";
 
     public function run() {}
 
