65c9e17aa08f35c133ab370b4417e342ba8f9d68
AbmSourav <keramotul.islam@gmail.com>
2023-01-09 12:46:10 +0600

    db configuration retrived from wp-config

--- Core/Database.php
+++ Core/Database.php
@@ -18,22 +18,12 @@ class Database
 
     private function getDbConfig()
     {
-        $envContent = $this->getEnvironments();
-        $lines = explode("\n", $envContent);
-        $data = [];
-
-        foreach($lines as $env) {
-            if (empty($env)) continue;
-        
-            $env_arr = explode('=', $env);
-            $data[$env_arr[0]] = $env_arr[1];
-        }
-        return $data;
-    }
-
-    private function getEnvironments()
-    {
-        $envFile = WPS_ROOT_DIR . '/.env';
-        return file_get_contents($envFile, true);
+        return [
+            "host"          => DB_HOST,
+            "dbname"        => DB_NAME,
+            "user"          => DB_USER,
+            "password"      => DB_PASSWORD,
+            "table_prefix"  => WPS_TABLE_PREFIX
+        ];
     }
 }
--- Core/PrepareData.php
+++ Core/PrepareData.php
@@ -8,11 +8,21 @@ class PrepareData
 
     public function map()
     {
+        if (! file_exists(WPS_ROOT_DIR . "/seeders/wpseeder.php")) {
+            print_r("\033[31m ERROR: /seeders/wpseeder.php not found in the parent folder of /vendor.\033[0m \n");
+            print_r(" Exit\n");
+            die();
+        }
         $seeder_list = require(WPS_ROOT_DIR . "/seeders/wpseeder.php");
 
         foreach ($seeder_list as $seeder) {
             if ('wpseeder' === $seeder) continue;
 
+            if (! file_exists(WPS_ROOT_DIR . "/seeders/$seeder.php")) {
+                print_r("\033[31m ERROR: $seeder.php not found.\033[0m");
+                print_r("\n Moving to the next exicution.\n");
+                continue;
+            }
             require(WPS_ROOT_DIR . "/seeders/" . $seeder . ".php");
             $seeder_object = new $seeder;
             $this->data[] = $seeder_object->run();
@@ -23,7 +33,7 @@ class PrepareData
 
     private function innerLoop($seeder, $row)
     {
-        if (1 === $row) {
+        if (1 === $row || empty($this->data)) {
             return;
         }
 
